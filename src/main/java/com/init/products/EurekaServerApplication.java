87e606fbec8761804d96ba104ce7b7e4cf1bb9e0
unknown <santiago16cfa@outlook.com>
2024-02-09 11:04:49 -0600

    subiendo el archivo a mi github

---
+++ src/main/java/com/init/products/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.init.products;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+@Configurable
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
